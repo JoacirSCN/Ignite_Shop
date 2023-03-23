9b3b68ae1ae966ef79993553f32cc5ee7d36fc78
joacir <joacirprog@gmail.com>
2023-03-23 20:49:49 -0300

    Finished

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,30 +1,108 @@
-import { useRouter } from 'next/router'
+import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
+import { useState } from 'react'
+// import { useRouter } from 'next/router'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
-} from '@/src/styles/pages/product'
+} from '../../styles/pages/product'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+    defaultPriceId: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  // const router = useRouter()
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      // Aplicação Interna Checkout
+      // router.push('/checkout')
+
+      // Aplicação externa
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // Conectar com uma ferramenta de observabilidade (Datadog / Sentry)
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar ao checkout')
+    }
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image alt="" src={product.imageUrl} width={520} height={480} />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>nomeDaCamina</h1>
-        <span>preço</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non,
-          repellat maiores impedit, laboriosam nihil distinctio vitae eveniet
-          beatae blanditiis quod quibusdam dolore nam hic, dignissimos ipsum
-          voluptatum. Voluptas, earum sed!
-        </p>
+        <p>{product.description}</p>
 
-        <button>Comprar Agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar Agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Buscar os produtos mais vendidos / mais acessados (máx 10)
+  return {
+    paths: [{ params: { id: 'prod_NZJxDW2yovJh1H' } }],
+    fallback: 'blocking', // Preferível utilizar o true
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}: any) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(Number(price.unit_amount) / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -42,4 +42,32 @@ export const ProductDetails = styled('div', {
     fontSize: '$2xl',
     color: '$green300',
   },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300',
+  },
+
+  button: {
+    marginTop: 'auto',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
+    },
+
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300',
+    },
+  },
 })
